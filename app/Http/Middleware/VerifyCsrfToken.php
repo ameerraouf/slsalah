59208868fcbf8133426348cf861a4d6b765b7bc0
abanoubtalaatr <38631117+abanoubtalaatr@users.noreply.github.com>
2023-10-23 14:05:05 +0300

    'wip'

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'click-pay-success'
+        'click-pay-success',
+        'click-pay-fail',
     ];
 }
