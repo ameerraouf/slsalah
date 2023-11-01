7ed23a7e68463de429228ebcd9882dbb6f4a60d6
abanoubtalaatr <38631117+abanoubtalaatr@users.noreply.github.com>
2023-11-01 18:51:52 +0200

    'wip'

--- resources/views/super-admin/chat/index.blade.php
+++ resources/views/super-admin/chat/index.blade.php
@@ -150,7 +150,7 @@
                         contentContainer.append(contentText);
 
              if (data.chat.file != null) {
-                var fileLink = $('<a>').attr('href', data.chat.file).attr('target', '_blank').addClass('btn-primary px-2 rounded d-block w-50 text-end').text('المرفق');
+                var fileLink = $('<a>').attr('href', data.chat.file).attr('target', '_blank').addClass('btn-primary px-2 rounded d-block w-25 text-end').text('المرفق');
              }
 
             if (data.chat.audio != null) {
@@ -271,7 +271,7 @@
 
 
                         if (response.data.file != null) {
-                             var fileLink = $('<a>').attr('href', response.data.file).attr('target', '_blank').addClass('btn-primary px-2 rounded').text('المرفق');
+                             var fileLink = $('<a>').attr('href', response.data.file).attr('target', '_blank').addClass('btn-primary px-2 rounded  d-block w-25').text('المرفق');
                          }
 
 
