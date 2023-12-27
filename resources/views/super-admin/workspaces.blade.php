f7d02bab90134c57c62f1d598881a5af90bc9bb1
unknown <belalwesam3@gmail.com>
2023-12-28 01:23:02 +0300

    prenet duplicate admin workspace

--- resources/views/super-admin/workspaces.blade.php
+++ resources/views/super-admin/workspaces.blade.php
@@ -3,7 +3,7 @@
     <div class=" row mb-2">
         <div class="col">
             <h5 class=" text-secondary fw-bolder">
-                {{__('Workspaces')}}
+                {{ __('Workspaces') }}
             </h5>
         </div>
     </div>
@@ -15,69 +15,73 @@
                     <div class="table-responsive p-0">
                         <table class="table align-items-center mb-0" id="cloudonex_table">
                             <thead>
-                            <tr>
-                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">{{__('Workspace Name')}}</th>
-                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">{{__('Created_at')}}</th>
-                                <th class=" text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">{{__('Plan')}}</th>
-                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">{{__('Status')}}</th>
+                                <tr>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        {{ __('Workspace Name') }}</th>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                        {{ __('Created_at') }}</th>
+                                    <th class=" text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                        {{ __('Plan') }}</th>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        {{ __('Status') }}</th>
 
-                                <th class=" text-uppercase text-secondary text-xxs opacity-7">{{__('Action')}}</th>
-                            </tr>
+                                    <th class=" text-uppercase text-secondary text-xxs opacity-7">{{ __('Action') }}</th>
+                                </tr>
                             </thead>
                             <tbody class = "target-table">
-                            @foreach($workspaces as $workspace)
-                                <tr @if($user->super_admin && $workspace->id === $user->workspace_id) class = "bg-success" id="admin-wokspace-row" @endif>
-                                    <td>
-                                        <div class="d-flex px-2 py-1">
-                                            <div class="d-flex flex-column justify-content-center">
-                                                <h6 class="mb-0 text-sm">{{$workspace->name}} </h6>
-                                                <p class="text-xs text-secondary mb-0"></p>
+                                @foreach ($workspaces as $workspace)
+                                    <tr @if ($user->super_admin && $workspace->id === $user->workspace_id) class = "bg-success admin-wokspace-row" @endif>
+                                        <td>
+                                            <div class="d-flex px-2 py-1">
+                                                <div class="d-flex flex-column justify-content-center">
+                                                    <h6 class="mb-0 text-sm">{{ $workspace->name }} </h6>
+                                                    <p class="text-xs text-secondary mb-0"></p>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p class="text-xs font-weight-bold mb-0">
-                                            {{(\App\Supports\DateSupport::parse($workspace->created_at))->format(config('app.date_time_format'))}}
-                                        </p>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs font-weight-bold mb-0">
+                                                {{ \App\Supports\DateSupport::parse($workspace->created_at)->format(config('app.date_time_format')) }}
+                                            </p>
 
-                                    </td>
-                                    <td class="text-xs text-purple text-uppercase font-weight-bold mb-0">    @if($workspace->id !== $user->workspace_id)
-                                            @if(isset($plans[$workspace->plan_id]))
-                                                {{$plans[$workspace->plan_id]->name}}
-                                            @endif
-                                        @else
-                                            <p class="text-xs font-weight-bold mb-0">{{__('super admin')}}</p>
-                                        @endif
-
-                                    </td>
-                                    <td class="align-middle text-sm">
-                                        @if($workspace->id !== $user->workspace_id)
-                                            @if($workspace->subscribed)
-                                                <span class="badge badge-sm bg-success-light text-success">{{__('Subscribed')}}</span>
+                                        </td>
+                                        <td class="text-xs text-purple text-uppercase font-weight-bold mb-0">
+                                            @if ($workspace->id !== $user->workspace_id)
+                                                @if (isset($plans[$workspace->plan_id]))
+                                                    {{ $plans[$workspace->plan_id]->name }}
+                                                @endif
                                             @else
-                                                <span class="badge badge-sm bg-pink-light text-danger">{{__('Not Subscribed')}}</span>
+                                                <p class="text-xs font-weight-bold mb-0">{{ __('super admin') }}</p>
                                             @endif
-                                        @endif
 
-                                    </td>
-                                    <td class="align-middle">
-                                        @if($workspace->id !== $user->workspace_id)
-                                            <a class="btn btn-link text-dark px-3 mb-0"
-                                               href="/edit-workspace?id={{$workspace->id}}"><i
-                                                    class=" text-dark fas fa-pencil-alt me-2"></i>{{__('Edit')}}</a>
+                                        </td>
+                                        <td class="align-middle text-sm">
+                                            @if ($workspace->id !== $user->workspace_id)
+                                                @if ($workspace->subscribed)
+                                                    <span
+                                                        class="badge badge-sm bg-success-light text-success">{{ __('Subscribed') }}</span>
+                                                @else
+                                                    <span
+                                                        class="badge badge-sm bg-pink-light text-danger">{{ __('Not Subscribed') }}</span>
+                                                @endif
+                                            @endif
 
+                                        </td>
+                                        <td class="align-middle">
+                                            @if ($workspace->id !== $user->workspace_id)
+                                                <a class="btn btn-link text-dark px-3 mb-0"
+                                                    href="/edit-workspace?id={{ $workspace->id }}"><i
+                                                        class=" text-dark fas fa-pencil-alt me-2"></i>{{ __('Edit') }}</a>
+                                            @endif
 
-                                        @endif
-
-                                        @if($workspace->id !== $user->workspace_id)
-
-                                            <a class="btn btn-link text-danger text-gradient px-3 mb-0"
-                                               href="/delete-workspace/{{$workspace->id}}"><i
-                                                    class="far fa-trash-alt me-2"></i>{{__('Delete')}}</a>
-                                        @endif
-                                    </td>
-                                </tr>
-                            @endforeach
+                                            @if ($workspace->id !== $user->workspace_id)
+                                                <a class="btn btn-link text-danger text-gradient px-3 mb-0"
+                                                    href="/delete-workspace/{{ $workspace->id }}"><i
+                                                        class="far fa-trash-alt me-2"></i>{{ __('Delete') }}</a>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -89,9 +93,11 @@
 @section('script')
     <script>
         "use strict";
-        $(document).ready(function () {
-            if ($('#admin-wokspace-row').length > 0) {
-                $('.target-table').prepend($('#admin-wokspace-row').prop('outerHTML'))
+        $(document).ready(function() {
+            if ($('.admin-wokspace-row').length > 0) {
+                $('.target-table').prepend($('.admin-wokspace-row').prop('outerHTML'))
+                $('.admin-wokspace-row')[1].remove()
+
             }
             $('#cloudonex_table').DataTable({
                 language: {
