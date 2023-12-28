125179da0f0aca58ed04b730bebff2d66164e559
unknown <belalwesam3@gmail.com>
2023-12-28 15:30:38 +0300

    note 43 the video icon

--- resources/views/layouts/super-admin-portal.blade.php
+++ resources/views/layouts/super-admin-portal.blade.php
@@ -127,10 +127,7 @@
             <li class="nav-item">
 
                 <a class="nav-link @if(basename(parse_url(url()->current(), PHP_URL_PATH)) =='video' || request()->path() === 'video/create' || Str::contains(request()->path() , 'edit')) active @endif" href="/video">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell">
-                        <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-                        <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-                    </svg>
+                    <i class="fas fa-video"></i>
                     <span class="nav-link-text ms-3">{{__('control_video')}}</span>
                 </a>
             </li>
