e45edec7062d1dcf9ce14f5b3f6d08510304cc0f
unknown <belalwesam3@gmail.com>
2024-01-04 18:24:08 +0300

    note 52

--- resources/views/layouts/super-admin-portal.blade.php
+++ resources/views/layouts/super-admin-portal.blade.php
@@ -168,10 +168,7 @@
 
             <li class="nav-item">
                 <a class="nav-link @if(basename(parse_url(url()->current(), PHP_URL_PATH)) =='subscriptions-all' || isset($selected_navigation) && $selected_navigation == 'show_details_of_subscription') active @endif" href="/subscriptions-all">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell">
-                        <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-                        <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-                    </svg>
+                    <i class="fas fa-dollar-sign"></i>
                     <span class="nav-link-text ms-3">الاشتراكات</span>
                 </a>
             </li>
