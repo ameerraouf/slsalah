7228c71e71829797f1424beac54676b4fe383064
unknown <belalwesam3@gmail.com>
2024-01-07 13:10:04 +0300

    formatting

--- resources/views/actions/notes.blade.php
+++ resources/views/actions/notes.blade.php
@@ -15,7 +15,7 @@
             <div class="col-md-4 mb-4">
                 <div class="card">
                     @if(!empty($note->cover_photo))
-                        <img src="{{PUBLIC_DIR}}/uploads/{{$note->cover_photo}}" class="card-img-top" alt="...">
+                        <img src="{{PUBLIC_DIR}}/uploads/{{$note->cover_photo}}" class="card-img-top img-fluid" alt="...">
                     @endif
 
                     <div class="card-body">
