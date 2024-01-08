f61f6ada16673b0fdee392299fb34374112ed139
unknown <belalwesam3@gmail.com>
2024-01-08 22:57:46 +0300

    notification blank

--- resources/views/user_notification.blade.php
+++ resources/views/user_notification.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.'.($layout ?? 'primary'))
+@extends('layouts.' . ($layout ?? 'primary'))
 <style>
-    .font-size-12{
+    .font-size-12 {
         font-size: 12px;
     }
 </style>
@@ -27,32 +27,33 @@
     </div>
     <div class="">
         <div class="row mt-lg-0 mt-4 d-flex">
-
-            @foreach($notifications as $notification)
-
-                @if($notification['data']['notification_type'] == 'subscription')
-                    <a href="{{route('user.package')}}">
+            @forelse ($notifications as $notification)
+                @if ($notification['data']['notification_type'] == 'subscription')
+                    <a href="{{ route('user.package') }}">
                         <div></div>
-                        <div class="d-flex p-2 rounded flex-wrap border align-content-between cursor-pointer @if(is_null($notification->read_at)) bg-secondary text-white @endif" onclick="readNotification(this)">
-                           <div class="col-3">
-                            <input type="hidden" name="notification_id" value="{{ $notification->id }}">
-                            <strong class="mx-1 font-size-12">{{ $notification['data']['type'] }} </strong>
-                           </div>
+                        <div class="d-flex p-2 rounded flex-wrap border align-content-between cursor-pointer @if (is_null($notification->read_at)) bg-secondary text-white @endif"
+                            onclick="readNotification(this)">
+                            <div class="col-3">
+                                <input type="hidden" name="notification_id" value="{{ $notification->id }}">
+                                <strong class="mx-1 font-size-12">{{ $notification['data']['type'] }} </strong>
+                            </div>
                             <div class="col-3">
                                 @php
                                     $plan = \App\Models\SubscriptionPlan::find($notification['data']['plan']['id']);
                                 @endphp
 
-                                <strong class="font-size-12">اسم الباقة : <strong class="font-size-12">{{$plan? $plan->name:""}}</strong> </strong>
+                                <strong class="font-size-12">اسم الباقة : <strong
+                                        class="font-size-12">{{ $plan ? $plan->name : '' }}</strong> </strong>
                             </div>
 
                             <div class="col-4">
-                                <strong class="mx-1 font-size-12">تاريخ ووقت الاشتراك :{{\Carbon\Carbon::parse($notification['data']['subscribe']['created_at'])->format('Y-m-d m:h:s')}}</strong>
+                                <strong class="mx-1 font-size-12">تاريخ ووقت الاشتراك
+                                    :{{ \Carbon\Carbon::parse($notification['data']['subscribe']['created_at'])->format('Y-m-d m:h:s') }}</strong>
                             </div>
 
                             <div class="co-2">
                                 <span>
-                                    @if(is_null($notification->read_at))
+                                    @if (is_null($notification->read_at))
                                         <span class="mx-2 font-size-12">اشعار جديد</span>
                                     @else
                                         <span class="font-size-12">اشعار مرئي</span>
@@ -62,30 +63,33 @@
 
                         </div>
                     </a>
-                 @else
-                    <a href="{{route('user.video.show', $notification['data']['video'])}}">
-                    <div class="d-flex p-2 rounded flex-wrap border align-content-between cursor-pointer @if(is_null($notification->read_at)) bg-secondary text-white @endif" onclick="readNotification(this)">
-                        <div class="col-3">
-                            <input type="hidden" name="notification_id" value="{{ $notification->id }}">
-                            <strong class="mx-1">{{ $notification['data']['type'] }} </strong>
-                        </div>
+                @else
+                    <a href="{{ route('user.video.show', $notification['data']['video']) }}">
+                        <div class="d-flex p-2 rounded flex-wrap border align-content-between cursor-pointer @if (is_null($notification->read_at)) bg-secondary text-white @endif"
+                            onclick="readNotification(this)">
+                            <div class="col-3">
+                                <input type="hidden" name="notification_id" value="{{ $notification->id }}">
+                                <strong class="mx-1">{{ $notification['data']['type'] }} </strong>
+                            </div>
 
-{{--                        <strong>اسم الباقة : <strong>{{$notification->data['plan']? $notification->data['plan']['name']:""}}</strong> |</strong>--}}
-{{--                        <strong class="mx-1">تاريخ الاشتراك :{{\Carbon\Carbon::parse($notification['data']['subscription']['created_at'])->format('Y-m-d m:h:s')}}</strong>--}}
-                        <div class="col-3">
-                            <span>
-                                @if(is_null($notification->read_at))
-                                    <span class="mx-2">اشعار جديد</span>
-                                @else
-                                    <span>اشعار مرئي</span>
-                                @endif
-                            </span>
+                            {{--                        <strong>اسم الباقة : <strong>{{$notification->data['plan']? $notification->data['plan']['name']:""}}</strong> |</strong> --}}
+                            {{--                        <strong class="mx-1">تاريخ الاشتراك :{{\Carbon\Carbon::parse($notification['data']['subscription']['created_at'])->format('Y-m-d m:h:s')}}</strong> --}}
+                            <div class="col-3">
+                                <span>
+                                    @if (is_null($notification->read_at))
+                                        <span class="mx-2">اشعار جديد</span>
+                                    @else
+                                        <span>اشعار مرئي</span>
+                                    @endif
+                                </span>
+                            </div>
                         </div>
-                    </div>
                     </a>
                 @endif
 
-            @endforeach
+            @empty
+                <h4 class="my-2"> لا يوجد اشعارات حاليا </h4>
+            @endforelse
         </div>
     </div>
 
@@ -97,25 +101,27 @@
         $(element).removeClass('text-white');
         $(element).find('span').text('  اشعار مرئي ');
 
-    var notificationId = $(element).find('input[name="notification_id"]').val();
-console.log('the id is '+ notificationId);
-    $.ajax({
-        url: "/user/notifications/"+ notificationId,
-        type: 'POST',
-        data: { notification_id: notificationId },
-          headers: {
-    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-  },
-        success: function(response) {
-             var countElement = $('#user_notification_count');
-            var count = parseInt(countElement.text());
-            if (!isNaN(count) && count > 0) {
-                countElement.text(count - 1);
+        var notificationId = $(element).find('input[name="notification_id"]').val();
+        console.log('the id is ' + notificationId);
+        $.ajax({
+            url: "/user/notifications/" + notificationId,
+            type: 'POST',
+            data: {
+                notification_id: notificationId
+            },
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            success: function(response) {
+                var countElement = $('#user_notification_count');
+                var count = parseInt(countElement.text());
+                if (!isNaN(count) && count > 0) {
+                    countElement.text(count - 1);
+                }
+            },
+            error: function(xhr, status, error) {
+                console.error('Error marking notification as read:', error);
             }
-        },
-        error: function(xhr, status, error) {
-            console.error('Error marking notification as read:', error);
-        }
-    });
+        });
     }
 </script>
