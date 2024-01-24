1b9fca2bf156bef926bca9e41cab1c592b04c3b6
unknown <belalwesam3@gmail.com>
2024-01-24 12:33:45 +0300

    the translaion for brain storm delete

--- resources/views/plans/brainstorm-list.blade.php
+++ resources/views/plans/brainstorm-list.blade.php
@@ -73,7 +73,7 @@
     <script>
         $('body').on('click', '.delete-btn', function(e) {
             e.preventDefault()
-            var result = window.confirm("Are you sure you want to proceed?");
+            var result = window.confirm("هل أنت متأكد من الحذف؟");
             let targetLink = $(this).attr('href')
             if (result) {
                 window.location.href = targetLink;
