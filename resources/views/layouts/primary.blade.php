7d798f89fcccd77b8eb9504adf54f7fe25aacc12
unknown <belalwesam3@gmail.com>
2024-01-24 12:29:29 +0300

    the porter 5 pronnounceation

--- resources/views/layouts/primary.blade.php
+++ resources/views/layouts/primary.blade.php
@@ -148,7 +148,7 @@
                     <a class="nav-link  @if(($selected_navigation ?? '') === 'porter') active @endif" href="/porter-models">
 
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-life-buoy"><circle cx="12" cy="12" r="10"></circle><circle cx="12" cy="12" r="4"></circle><line x1="4.93" y1="4.93" x2="9.17" y2="9.17"></line><line x1="14.83" y1="14.83" x2="19.07" y2="19.07"></line><line x1="14.83" y1="9.17" x2="19.07" y2="4.93"></line><line x1="14.83" y1="9.17" x2="18.36" y2="5.64"></line><line x1="4.93" y1="19.07" x2="9.17" y2="14.83"></line></svg>
-                        <span class="nav-link-text ms-3">{{__('Porter 5-F Model')}}</span>
+                        <span class="nav-link-text ms-3">{{__('Porter Five test')}}</span>
                     </a>
                 </li>
 
